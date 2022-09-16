98438dec0343a9d9bb9ca3e8a96571796b3787ec
shadow-yuan <shadow_yuan@qq.com>
2022-09-16 10:08:43 +0800

    chore: 清理单元测试

--- test/array_test.cc
+++ test/array_test.cc
@@ -1,14 +1,7 @@
 
 #include <iostream>
-#include "src/file_reader.h"
 
 int main(int argc, char *[]) {
-    TOML::FileReader reader(__FILE__);
-
-    for (const auto &ctx : reader) {
-        std::cout << ctx.Line() << ":" << ctx.Data() << std::endl;
-    }
-
     std::cout << "Hello World" << std::endl;
     return 0;
 }
---
+++ test/integer_test.cc
@@ -0,0 +1,5 @@
+
+#include <iostream>
+#include "toml/toml.h"
+
+int main(int argc, char *[]) { return 0; }
